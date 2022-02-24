6904ea018e5d160938e9fde93eb14eda8a5819f1
Sergey <alozhka@yahoo.com>
2022-02-25 00:16:18 +0300

    almost done 33.php

--- lw3/33.php
+++ lw3/33.php
@@ -2,6 +2,16 @@
   header("Content-Type:text/plain");
   $pas = $_GET["password"];
   $reability = 0;
-  $reabilty = (int) strlen($pas);
+  $len = strlen($pas);
+  //unique symbols +
+  $cntUnique = strlen(count_chars($pas, 3));;
+  //digits +
+  $cntDigits = strlen(preg_replace('/[^0-9]/ui', '', $pas));
+  //upper&lowerCase +
+  $cntUpCase = strlen(preg_replace('/[^A-ZА-ЯЁ]/', '', $pas));
+  $cntLowCase = strlen(preg_replace('/[A-ZА-ЯЁ]/', '', $pas));
+  //reability
+  $reability = 4*$cntUnique + 4*$cntDigits + 2*($len - $cntUpCase) 
+  + 2*($len - $cntLowCase);
   echo 'password: ', $pas,"\n";
-  echo 'reability: ', $reability;
+  echo 'reability: ', $reabilty," ";
