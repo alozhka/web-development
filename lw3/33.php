23ba389e191f5c7743f14f51bdeb8e3735605d29
Sergey <alozhka@yahoo.com>
2022-03-08 18:59:06 +0300

    new 33

--- lw3/33.php
+++ lw3/33.php
@@ -10,8 +10,17 @@
   //upper&lowerCase +
   $cntUpCase = strlen(preg_replace('/[^A-ZА-ЯЁ]/', '', $pas));
   $cntLowCase = strlen(preg_replace('/[A-ZА-ЯЁ]/', '', $pas));
+  $cntReccurent = $len - $cntUnique;
   //reability
   $reability = 4*$cntUnique + 4*$cntDigits + 2*($len - $cntUpCase) 
-  + 2*($len - $cntLowCase);
-  echo 'password: ', $pas,"\n";
-  echo 'reability: ', $reabilty," ";
+  + 2*($len - $cntLowCase) - 2*$cntRecurrent;
+  if ($cntDigits == 0)
+  {
+      $reability -= $cntDigits;
+  }
+  if ($cntUpCase == 0 & $cntLowCase == 0)
+  {
+      $reability -= $cntDigits;
+  }
+  echo "password: ", $pas,"\n";
+  echo "reability: ", $reabilty,"\n";
