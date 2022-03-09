39dee9ac1b012cfeb3013f45e0fb63f3dd9afb13
Sergey <alozhka@yahoo.com>
2022-03-09 23:38:45 +0300

    working via phpStorm

--- lw3/33.php
+++ lw3/33.php
@@ -1,26 +1,42 @@
 <?php
-  header("Content-Type:text/plain");
-  $pas = $_GET["password"];
-  $reability = 0;
-  $len = strlen($pas);
-  //unique symbols +
-  $cntUnique = strlen(count_chars($pas, 3));;
-  //digits +
-  $cntDigits = strlen(preg_replace('/[^0-9]/ui', '', $pas));
-  //upper&lowerCase +
-  $cntUpCase = strlen(preg_replace('/[^A-ZА-ЯЁ]/', '', $pas));
-  $cntLowCase = strlen(preg_replace('/[A-ZА-ЯЁ]/', '', $pas));
-  $cntReccurent = $len - $cntUnique;
-  //reability
-  $reability = 4*$cntUnique + 4*$cntDigits + 2*($len - $cntUpCase) 
-  + 2*($len - $cntLowCase) - 2*$cntRecurrent;
-  if ($cntDigits == 0)
-  {
-      $reability -= $cntDigits;
-  }
-  if ($cntUpCase == 0 & $cntLowCase == 0)
-  {
-      $reability -= $cntDigits;
-  }
-  echo "password: ", $pas,"\n";
-  echo "reability: ", $reabilty,"\n";
+
+header("Content-Type:text/plain");
+$pas = $_GET["password"];
+$reability = 0;
+$len = strlen($pas);
+//unique symbols +
+$cntUnique = strlen(count_chars($pas, 3));;
+//digits +
+$cntDigits = strlen(preg_replace("/[^0-9]/ui", '', $pas));
+//upper&lowerCase +
+$cntUpCase = strlen(preg_replace("/[^A-ZА-ЯЁ]/", '', $pas));
+$cntLowCase = strlen(preg_replace("/[A-ZА-ЯЁ]/", '', $pas));
+$cntRecurent = $len - $cntUnique;
+//reability
+$reability = 4*$cntUnique + 4*$cntDigits + 2*($len - $cntUpCase) + 2*($len - $cntLowCase) - 2*$cntRecurent;
+if ($len <> 0) //есть хоть что-то
+{
+    $reability += 4*$len;
+}
+if ($cntDigits <> 0) //есть цифры
+{
+    $reability += 4*$cntDigits;
+}
+if ($cntUpCase <> 0) //верхний рег
+{
+    $reability += 2*$cntRecurent;
+}
+if ($cntLowCase <> 0) //нижний рег
+{
+    $reability += 2*$cntRecurent;
+}
+if ($cntDigits == $len) //только цифры
+{
+    $reability -= $cntDigits;
+}
+if (($cntUpCase + $cntLowCase) == $len) //только буквы
+{
+    $reability -= $cntDigits;
+}
+echo "password: ", $pas,"\n";
+echo "reability: ", $reability;
