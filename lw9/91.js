469ec7573c0b7b5ee761dee13d5eefaeded3e521
SergeyLozhkin <100339481+alozhka@users.noreply.github.com>
2022-04-15 10:30:30 +0300

    alozhka
    lw9

--- lw9/91.js
+++ lw9/91.js
@@ -1,5 +1,38 @@
 {
-  function isPrimeNumber(Numbers) {
-      if 
+  function PrimeCheckBint(Number) {
+      let i = 2n
+      for (i; i <= Math.sqrt(Number); i++)
+          if (Number % i === 0n) return false
+      return true
+  }
+  function PrimeCheckNum(Number) {
+      let i = 2n
+      for (i; i <= Math.sqrt(Number); i++)
+          if (Number % i === 0) return false
+      return true
+    }
+  function isPrimeNumber(Number) {
+      const type = typeof (Number)
+      //bigint
+      if (type === 'bigint')
+          if (Number <= 2n) return Number + " is not prime number"
+          else if (PrimeCheckBint(Number))
+              return Number + " is prime number"
+          else
+              return Number + " is not prime number"
+      //number
+      if (type === 'number')
+          if (Number <= 2)
+              return Number + " is not prime number"
+          else if (PrimeCheckNum(Number))
+              return Number + " is prime number"
+          else
+              return Number + " is not prime number"
+      //object
+      if (type === 'object') {
+          const len = Object.length(Number)
+          const objType = typeof(Number[0])
+      //     if (objType === 'bigint')
+      // }
   }
 }
