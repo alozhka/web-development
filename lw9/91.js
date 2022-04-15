41582e1bd79435e18a0cfea723bae407b749c358
SergeyLozhkin <100339481+alozhka@users.noreply.github.com>
2022-04-15 11:58:00 +0300

    Update 91 alg

--- lw9/91.js
+++ lw9/91.js
@@ -1,38 +1,22 @@
 {
-  function PrimeCheckBint(Number) {
-      let i = 2n
-      for (i; i <= Math.sqrt(Number); i++)
-          if (Number % i === 0n) return false
-      return true
-  }
-  function PrimeCheckNum(Number) {
-      let i = 2n
-      for (i; i <= Math.sqrt(Number); i++)
-          if (Number % i === 0) return false
-      return true
+    function PrimeCheck(Number) {
+        if (Number <= 2) return true
+        for (let i = 2; i <= Math.sqrt(Number); i++)
+            if (Number % i === 0) return false
+        return true
     }
-  function isPrimeNumber(Number) {
-      const type = typeof (Number)
-      //bigint
-      if (type === 'bigint')
-          if (Number <= 2n) return Number + " is not prime number"
-          else if (PrimeCheckBint(Number))
-              return Number + " is prime number"
-          else
-              return Number + " is not prime number"
-      //number
-      if (type === 'number')
-          if (Number <= 2)
-              return Number + " is not prime number"
-          else if (PrimeCheckNum(Number))
-              return Number + " is prime number"
-          else
-              return Number + " is not prime number"
-      //object
-      if (type === 'object') {
-          const len = Object.length(Number)
-          const objType = typeof(Number[0])
-      //     if (objType === 'bigint')
-      // }
-  }
+
+    function isPrimeNumber(Number) {
+        const type = typeof (Number)
+        switch (type) {
+            case "number":
+                if (PrimeCheck(Number))
+                    return Number + " is Prime number"
+                else
+                    return Number + " is not Prime number"
+            case "object": {
+
+            }
+        }
+    }
 }
