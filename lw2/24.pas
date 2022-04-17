6d7e987efd10648b94f452f7ad1c6b201732592e
Sergey <aLozhka@yahoo.com>
2022-04-17 22:58:27 +0300

    24 is done

--- lw2/24.pas
+++ lw2/24.pas
@@ -1,10 +1,28 @@
 PROGRAM WorkWithQueryString(INPUT, OUTPUT);
 USES
   DOS;
-FUNCTION GetQueryStringParameter(Key: STRING): STRING;
-BEGIN {GetQueryStringParameter}
 
-END; {GetQueryStringParameter}
+FUNCTION GetQueryStringParameter(Key: STRING): STRING;
+VAR
+  Value, Query: STRING;
+  PosValue: INTEGER;
+BEGIN
+  Query := GetEnv('QUERY_STRING');
+  Value := '';
+  Key := Concat(Key, '=');
+  I := POS(Key, Query);
+  IF I > 0
+  THEN
+    BEGIN
+      WHILE I < Length(Query) + 1) AND (Query[I] <> '&')
+      DO
+        BEGIN
+          Value := Value + Query[I];
+          I := I + 1
+        END
+    END;
+  GetQueryStringParameter := Value
+END;
 
 BEGIN {WorkWithQueryString}
   WRITELN('Content-Type: text/plain');
