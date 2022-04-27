519126acde2c58b590098150baae5870d0c9ab2d
Sergey <aLozhka@yahoo.com>
2022-04-27 18:41:44 +0300

    83 is done

---
+++ lw8/request.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Request from Database</title>
+    <link rel="stylesheet" href="styles/request-form-styles.css">
+</head>
+<body>
+<?php
+include("request-form.html");
+?>
+</body>
+</html>
