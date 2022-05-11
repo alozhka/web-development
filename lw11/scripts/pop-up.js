4c3b03e33844c8e6a7ee83b849f38d9f2443635b
Sergey <aLozhka@yahoo.com>
2022-05-11 15:12:09 +0300

    merge troubles

--- lw11/scripts/pop-up.js
+++ lw11/scripts/pop-up.js
@@ -93,7 +93,6 @@ function main() {
         blackout.classList.remove('blackoutShow')
     }
 
-<<<<<<< HEAD
     const name = document.getElementById('ajax-form__name')
     const email = document.getElementById('ajax-from__email')
     const sendFormButton = document.getElementById('ajax-form')
@@ -118,10 +117,5 @@ function main() {
                 email: document.getElementById('ajax-form_email').value
             })
         }
-=======
-    let promise = fetch('register.php', [text/plain])
-    async function doFetch() {
-        const
->>>>>>> parent of bfff8fc (godness promise)
     }
 }
