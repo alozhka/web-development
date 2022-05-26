a9e41cfab0965a9d3906bd2b99c5991790582cd9
SergeyLozhkin <100339481+alozhka@users.noreply.github.com>
2022-05-26 11:20:48 +0300

    lw 12 working on

---
+++ lw12/sqript.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE university_db;
+USE university_db;
+CREATE TABLE students
+(
+	id 			INT AUTO_INCREMENT 	NOT NULL,
+	name 		VARCHAR(32) 		NOT NULL,
+	surnname 	VARCHAR(32) 		NOT NULL,
+    age 		INT					NOT NULL,
+    group_id	INT 				NOT NULL,
+    PRIMARY KEY(id)
+);
+CREATE TABLE students_group
+(
+	id			INT AUTO_INCREMENT 	NOT NULL,
+    faculty_id	INT					NOT NULL,
+    PRIMARY KEY(id)
+);
+CREATE TABLE faculty
+(
+	id 			INT AUTO_INCREMENT		NOT NULL,
+    name		VARCHAR(255),
+    PRIMARY KEY(id)
+);
