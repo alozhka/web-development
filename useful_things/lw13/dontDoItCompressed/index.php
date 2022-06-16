396e848979c4f825bf903e23779a9cf127afe6e9
SergeyLozhkin <100339481+alozhka@users.noreply.github.com>
2022-06-16 20:53:15 +0300

    final setup & compressed one

---
+++ useful_things/lw13/dontDoItCompressed/index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Don't do it</title>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width"/>
+    <style>
+      @import url(https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap);@import url(https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap);body{width:100%;margin:0}.main_wrap{margin:0;width:100%}.upper_frame{margin:0}.upper_frame_stripe{margin:30px auto 43px auto;display:flex;justify-content:space-between;max-width:1680px;padding:0 30px 0 30px;flex-wrap:wrap}.upper_frame_stripe_main{margin:9px 0 0 0;padding-right:32px}.upper_frame_stripe_sec{margin:0;display:flex;align-items:center;flex-wrap:wrap}.upper-frame-stripe-sec__questions{margin:0;padding-right:80px;width:520px;display:flex;justify-content:space-between}.upper_frame_button{margin:0;border-radius:10px;width:247px;height:55px;color:#05090c;background:#05090c}.upper_frame_stripe_button_text{margin:18px 0 17px 0;font-family:Montserrat,serif;font-style:normal;font-weight:600;font-size:16px;line-height:16px;text-align:center;color:#fff}.upper_frame_button_text{font-family:Montserrat,serif;font-style:normal;font-weight:600;font-size:16px;line-height:16px;text-align:center;color:#fff}.upper_frame_stripe_main_text_bl{width:203px;height:40px;top:5px;font-family:Montserrat,serif;font-style:normal;font-weight:700;font-size:31px;line-height:122.33%;letter-spacing:-.045em;color:#000}.upper_frame_stripe_main_text_pur{width:203px;height:40px;left:0;top:5px;font-family:Montserrat,serif;font-style:normal;font-weight:700;font-size:31px;line-height:122.33%;letter-spacing:-.045em;color:#ee58b6}.upper_frame_stripe_text{font-family:Montserrat,serif;font-style:normal;font-weight:500;font-size:16px;line-height:20px}.upper_frame_main{margin:0 auto 0 auto;max-width:1680px;padding:0 30px 0 30px;display:flex;justify-content:space-evenly;flex-wrap:wrap-reverse}.upper_frame_main_header{margin:180px 30px 5px 20px}.upper_frame_main_header_text_bl{font-family:Montserrat,serif;font-style:normal;font-weight:600;font-size:100px;line-height:122.35%;color:#000}.upper_frame_main_header_text_pur{font-family:Montserrat,serif;font-style:normal;font-weight:600;font-size:100px;line-height:122.35%;color:#ee58b6}.upper_frame_main_postext{margin:21px 0 46px 0;max-width:545px;width:95%;height:62px}.upper_frame_main_text{margin:0 auto;font-family:'Roboto Condensed',serif;font-style:normal;font-weight:400;font-size:26px;line-height:120%;color:rgba(0,0,0,.7)}.upper_frame_main_button{margin:0;border-radius:10px;color:#05090c;background:#05090c;width:332px;height:55px}.upper-frame__image{margin:0;max-width:100%;width:763px;height:638px}.black_frame{background:#000;margin:50px 0 0 0;padding:30px 0 30px 0;min-height:398px;width:100%;display:flex;align-items:center}.black_frame_collumns{margin:0 auto 0 auto;padding:0 5% 0 5%;max-width:1138px;height:100%;display:flex;justify-content:center;flex-wrap:wrap}.black_frame_col{margin:10px 0 10px 0;max-width:340px;max-height:145px;display:flex;flex-wrap:wrap;justify-content:center;align-items:center}.black_frame_column_img{margin:0 0 15px 0}.black_frame_text{font-family:'Roboto Condensed',serif;font-style:normal;font-weight:400;font-size:22px;line-height:120%;text-align:center;color:#fff}.black_frame_text_time{margin:0 0 0 2px;width:90%;text-align:center}.black_frame_text_notebook{margin:0 auto;width:100%;text-align:center}.black_frame_text_target{margin:0 auto;width:100%;text-align:center}.sidebar_frame{margin:72px auto 0 auto;max-width:1135px;display:flex;justify-content:center;flex-wrap:wrap;flex-direction:column}.sidebar_finances{margin:0 0 60px 0;display:flex;justify-content:center;flex-wrap:wrap}.sidebar_mind-blow{margin:0;display:flex;justify-content:center;flex-wrap:wrap-reverse}.sidebar_left_img{margin:0;max-width:538px;width:100%}.sidebar-left__image{margin:0 auto}.sidebar_right_img{margin:5% 153px 0 82px;max-width:378px;width:100%}.sidebar-right__image{margin:0}.sidebar_finances_main{margin:225px 20px 0 0;max-width:574px}.sidebar_mind-blow_main{margin:284px 0 0 34px;max-width:574px}.sidebar_main_header{margin:0;font-family:Montserrat,serif;float:none;font-style:normal;font-weight:500;font-size:48px;line-height:126.06%}.sidebar_main_postext{margin-top:28px}.sidebar_main_text{font-family:'Roboto Condensed',serif;font-style:normal;font-weight:400;font-size:18px;line-height:135%;color:rgba(0,0,0,.7)}.table_frame{margin:15px 0 15px 0}.table-frame-head{padding:0 10px 0 10px;display:flex;justify-content:center;align-items:center}.table_frame-header{margin:0;display:flex;justify-content:center;flex-wrap:wrap}.table_frame-header-text-bl{margin-right:12px;font-family:Montserrat,serif;font-style:normal;font-weight:500;font-size:48px;line-height:59px;color:#000}.table_frame-header-text-pur{font-family:Montserrat,serif;font-style:normal;font-weight:500;font-size:48px;line-height:59px;color:#ee58b6}.table_frame-tables{margin:0 auto;max-width:1170px;display:flex;justify-content:center;flex-flow:row wrap}.table{max-width:358px;width:90%;height:190px;margin:15px;background:#000;border-radius:7px}.table-image{margin:31px 0 18px 40px}.table-postext{margin:0 30px 0 40px}.table-text{font-family:'Roboto Condensed',serif;font-style:normal;font-weight:400;font-size:18px;line-height:21px;color:#fff}.bottom_stripe{width:100%;height:108px;left:1px;display:flex;justify-content:center;align-items:center;background:#000}.bottom_stripe-text{margin:0 auto}.bottom_stripe-text-wh{font-family:Montserrat,serif;font-style:normal;font-weight:700;font-size:31px;line-height:122.33%;letter-spacing:-.045em;color:#fff}@media screen and (max-width:907px){.upper_frame_stripe{margin:30px 0 20px 0}.upper-frame-stripe-sec__questions{flex-direction:column;padding:0;width:auto;margin:0 auto 0 0}.upper_frame_stripe_button_text{font-size:14px}.upper_frame_main_header_text_bl{font-size:3em}.upper_frame_main_header_text_pur{font-size:3em}.upper_frame_stripe_sec{margin-right:0}.upper_frame_button{width:40%}.black_frame_text{font-size:20px}}@media screen and (max-width:610px){.sidebar_main_header{font-size:2em}.sidebar_main_text{font-size:1em}.table-text{font-size:1em}.table_frame-header-text-bl{font-size:1.8em}.table_frame-header-text-pur{font-size:1.8em}.upper_frame_stripe_button_text{font-size:10px}.upper_frame_stripe_text{font-size:14px}.upper_frame_stripe_sec{justify-content:space-between;width:50%}.upper_frame_stripe_sec{display:none}.upper_frame_main_header_text_bl{font-size:2em}.upper_frame_main_header_text_pur{font-size:2em}.upper_frame_main_text{font-size:.95em}.upper_frame_main_button{width:60%;margin:0 auto 0 auto;display:block}.upper_frame_main_postext{margin:10px 0 14px 0}.upper_frame_button_text{font-size:12px}.black_frame_text{font-size:14px}.sidebar-left__image{width:100%}.sidebar_right_img{width:80%;margin:0}}@media screen and (max-width:1131px){.sidebar_frame{margin:0}.sidebar_finances{margin-top:20px}.sidebar_finances_main{margin:20px 20px 0 10px}.sidebar_mind-blow_main{margin:20px}}@media screen and (max-width:1135px){.upper_frame_stripe_main{margin:9px auto 0 auto}.upper_frame_stripe_sec{margin:0 auto 0 auto}}@media screen and (max-width:1569px){.upper_frame_main_header{margin:0 30px 0 20px}.upper-frame__image{width:95%}}
+    </style>
+    <noscript class="loadLater">
+        <link rel="stylesheet" href="styles/secStyle.css"/>
+    </noscript>
+    <script type="application/javascript">
+        document.addEventListener("DOMContentLoaded", function(event) {
+            let loadLater = document.querySelector('.loadLater');
+            let head = document.querySelector('head');
+            if(loadLater && head){
+                head.insertAdjacentHTML('beforeend', loadLater.innerHTML);
+            }
+        })
+    </script>
+</head>
+<body>
+<div class="main_wrap">
+    <div class="upper_frame">
+        <div class="upper_frame_stripe">
+            <div class="upper_frame_stripe_main">
+                <a class="upper_frame_stripe_main_text_bl">Don`t </a>
+                <a class="upper_frame_stripe_main_text_pur">do </a>
+                <a class="upper_frame_stripe_main_text_bl">it</a>
+            </div>
+            <div class="upper_frame_stripe_sec">
+                <div class="upper-frame-stripe-sec__questions">
+                    <a class="upper_frame_stripe_text">Что будет на курсе?</a>
+                    <a class="upper_frame_stripe_text">Вопросы</a>
+                    <a class="upper_frame_stripe_text">Автор</a>
+                </div>
+                <button class="upper_frame_button">
+                    <a class="upper_frame_stripe_button_text">Записаться на курс</a>
+                </button>
+            </div>
+        </div>
+        <div class="upper_frame_main">
+            <div class="upper_frame_main_header">
+                <a class="upper_frame_main_header_text_bl">Не </a>
+                <a class="upper_frame_main_header_text_pur">делай </a>
+                <a class="upper_frame_main_header_text_bl">это</a>
+                <div class="upper_frame_main_postext">
+                    <p class="upper_frame_main_text">Онлайн-курс для творческих людей, о том, как управлять своим
+                        временем</p>
+                </div>
+                <button class="upper_frame_main_button">
+                    <a class="upper_frame_button_text">Записаться на курс</a>
+                </button>
+            </div>
+            <div class="upper_frame_img">
+                <img class="upper-frame__image" src="images/illustration.webp" alt="Иллюстрация: не делай это"/>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="black_frame">
+    <div class="black_frame_collumns">
+        <div class="black_frame_col">
+            <img class="black_frame_column_img"
+                 src="images/time.webp"
+                 alt="time image"
+                 loading="lazy"/>
+            <div class="black_frame_text_time">
+                <a class="black_frame_text ">Для тех, у кого слишком много идей и слишком мало времени</a>
+            </div>
+        </div>
+        <div class="black_frame_col">
+            <img class="black_frame_column_img"
+                 src="images/notebook.webp"
+                 alt="notebook image"
+                 loading="lazy"/>
+            <div class="black_frame_text_notebook">
+                <a class="black_frame_text">Метод «списка не дел», который позволит успевать и реализовывать</a>
+            </div>
+        </div>
+        <div class="black_frame_col">
+            <img class="black_frame_column_img"
+                 src="images/target.webp"
+                 alt="target image"
+                 loading="lazy"/>
+            <div class="black_frame_text_target">
+                <a class="black_frame_text">Курс научит творческих людей сосредоточиваться</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="main_wrap">
+    <div class="sidebar_frame">
+        <div class="sidebar_finances">
+            <div class="sidebar_left_img">
+                <img class="sidebar-left__image"
+                     src="images/finances.webp"
+                     alt="финансы"
+                     loading="lazy"/>
+            </div>
+            <div class="sidebar_finances_main">
+                <a class="sidebar_main_header">Ты не успеешь</a>
+                <div class="sidebar_main_postext">
+                    <a class="sidebar_main_text">Всех творческих людей объединяет одна проблема - отсутствие времени на
+                        реализацию идей. Как прибавить суткам часы, рассмотрим в нашем курсе.</a>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar_mind-blow">
+            <div class="sidebar_mind-blow_main">
+                <a class="sidebar_main_header">Опять дедлайн</a>
+                <div class="sidebar_main_postext">
+                    <a class="sidebar_main_text">В мире, где столько всего интересного, когда же успевать жить?</a>
+                </div>
+            </div>
+            <div class="sidebar_right_img">
+                <img class="sidebar-left__image"
+                     src="images/mind_blowing.webp"
+                     alt="Взрыв мозга"
+                     loading="lazy"/>
+            </div>
+        </div>
+    </div>
+
+    <div class="table_frame">
+        <div class="table-frame-head">
+            <div class="table_frame-header">
+                <a class="table_frame-header-text-bl">На курсе ты </a>
+                <a class="table_frame-header-text-pur">сможешь</a>
+            </div>
+        </div>
+        <div class="table_frame-tables">
+            <div class="table">
+                <img class="table-image"
+                     src="images/hand-1.svg"
+                     alt="hand 1"
+                     loading="lazy"/>
+                <div class="table-postext">
+                    <a class="table-text">Понять, что нужно делать, а что делать не стоит.</a>
+                </div>
+            </div>
+            <div class="table">
+                <img class="table-image"
+                     src="images/hand-2.svg"
+                     alt="hand 2"
+                     loading="lazy"/>
+                <div class="table-postext">
+                    <a class="table-text">Перестать себя искусственно ограничивать.</a>
+                </div>
+            </div>
+            <div class="table">
+                <img class="table-image"
+                     src="images/hand-3.svg"
+                     alt="hand 3"
+                     loading="lazy"/>
+                <div class="table-postext table-postext_low">
+                    <a class="table-text">Определить сильные стороны и начать использовать слабые.</a>
+                </div>
+            </div>
+            <div class="table">
+                <img class="table-image"
+                     src="images/hand-4.svg"
+                     alt="hand 4"
+                     loading="lazy"/>
+                <div class="table-postext">
+                    <a class="table-text">Научиться достигать любой цели в 3 понятных шага.</a>
+                </div>
+            </div>
+            <div class="table">
+                <img class="table-image"
+                     src="images/hand-5.svg"
+                     alt="hand 5"
+                     loading="lazy"/>
+                <div class="table-postext">
+                    <a class="table-text">Сотрудничать эффективно и с правильными людьми.</a>
+                </div>
+            </div>
+            <div class="table">
+                <img class="table-image"
+                     src="images/hand-6.svg"
+                     alt="hand 6"
+                     loading="lazy"/>
+                <div class="table-postext">
+                    <a class="table-text">Оптимизировать общение с клиентами и проведение совещаний.</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom_stripe">
+        <div class="bottom_stripe-text">
+            <a class="bottom_stripe-text-wh">Don`t </a>
+            <a class="upper_frame_stripe_main_text_pur">do </a>
+            <a class="bottom_stripe-text-wh">it</a>
+        </div>
+    </div>
+</div>
+<script src="scripts/pop-up.js" async></script>
+</body>
+</html>
